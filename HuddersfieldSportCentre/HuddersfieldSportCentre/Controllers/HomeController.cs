e9a2aaa6588e4e85ae8716817bbaef40a6fd7601
james moffatt <moffatt123@hotmail.co.uk>
2016-04-16 14:17:59 +0100

    Added a simple statistics page "About". Displays how many customers registered at a particular date. Will Improve on this at a later stage.

--- HuddersfieldSportCentre/HuddersfieldSportCentre/Controllers/HomeController.cs
+++ HuddersfieldSportCentre/HuddersfieldSportCentre/Controllers/HomeController.cs
@@ -3,21 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using HuddersfieldSportCentre.DataAccessLayer;
+using HuddersfieldSportCentre.ViewModels;
 
 namespace HuddersfieldSportCentre.Controllers
 {
     public class HomeController : Controller
     {
+        private SportContext db = new SportContext();
+
         public ActionResult Index()
         {
+
             return View();
         }
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            IQueryable<EnrollmentDateGroup> data = from customer in db.Customers
+                                                   group customer by customer.EnrollmentDate into dateGroup
+                                                   select new EnrollmentDateGroup()
+                                                   {
+                                                       EnrollmentDate = dateGroup.Key,
+                                                       CustomerCount = dateGroup.Count()
+                                                   };
 
-            return View();
+            return View(data.ToList());
+        }
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
         }
 
         public ActionResult Contact()
