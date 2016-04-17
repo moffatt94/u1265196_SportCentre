80dcc33b40d8555dbf0d68d5f6485fe658aa02e1
james moffatt <moffatt123@hotmail.co.uk>
2016-04-17 16:32:50 +0100

    Executed First Data Migration and Created more models to produce a more complex Data Structure.

---
+++ HuddersfieldSportCentre/HuddersfieldSportCentre/Models/CourtAssignment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HuddersfieldSportCentre.Models
+{
+    public class CourtAssignment
+    {
+        [Key]
+        [ForeignKey("Trainer")]
+        public int CourseID { get; set; }
+        [Display(Name = "Court Location")]
+        public string Location { get; set; }
+
+        public virtual Trainer Trainer { get; set; }
+    }
+}
