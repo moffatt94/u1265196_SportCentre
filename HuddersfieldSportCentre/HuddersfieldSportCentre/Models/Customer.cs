da301dbfc407b8ca69cae85871dc7eb06a3d5243
james moffatt <moffatt123@hotmail.co.uk>
2016-04-29 22:05:00 +0100

    made changes to the design of the website.

--- HuddersfieldSportCentre/HuddersfieldSportCentre/Models/Customer.cs
+++ HuddersfieldSportCentre/HuddersfieldSportCentre/Models/Customer.cs
@@ -17,6 +17,8 @@ namespace HuddersfieldSportCentre.Models
         [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
         [Display(Name = "First Name")]
         public string FirstName { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         [Display(Name = "Enrollment Date")]
         public DateTime EnrollmentDate { get; set; }
         [Display(Name = "Email Address")]
--- HuddersfieldSportCentre/HuddersfieldSportCentre/ViewModels/EnrollmentDateGroup.cs
+++ HuddersfieldSportCentre/HuddersfieldSportCentre/ViewModels/EnrollmentDateGroup.cs
@@ -9,6 +9,7 @@ namespace HuddersfieldSportCentre.ViewModels
     public class EnrollmentDateGroup
     {
         [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM}", ApplyFormatInEditMode = true)]
         public DateTime? EnrollmentDate { get; set; }
 
         public int CustomerCount { get; set; }
